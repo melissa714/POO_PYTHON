021aa9409b8c11cd53f804c15474c347b3163669
ange melissa <nguessanmelissa01@gmail.com>
2023-02-06 23:41:03 +0000

    instanciation

--- create_class.py
+++ create_class.py
@@ -1,6 +1,46 @@
 class Cake: 
-    def __init__(self,flavor) :
+    def __init__(self,flavor,color="jaune") :
         self.flavor = flavor
+        self.color=color
 
-    def cut_in_part(self): 
-        print("le gateau est coupé en 4 part")
+
+    def cut_in_part(self,color): 
+        print("le gateau est coupé en 4 part",color)
+
+
+    def eat_cake(self):
+        print("miam miam")
+
+
+
+
+gateau=Cake("vanille")
+print(gateau.flavor)
+cake=Cake("chocolate")
+print(cake.flavor)
+
+print(cake.cut_in_part("vert"))
+
+
+
+
+
+
+class Student:
+    def __init__(self,name,roll_num,marks):
+        self.name=name 
+        self.roll_num=roll_num 
+        self.marks=marks
+        
+
+class Rectangle:
+    def __init__(self, length, width, color="red"):
+        self.length = length
+        self.width = width
+        self.color = color
+
+
+rect1 = Rectangle(4, 2, color="blue")
+
+rectangle=Rectangle(10,12)
+print(rectangle.color)
